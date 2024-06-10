751cce3b7a0b7b4f97611bd754664bac4ec79a37
Ethan Kao <ethan.kao@gmail.com>
2024-06-09 17:17:54 -0700

    Fullscreen menu

--- js/navbar.js
+++ js/navbar.js
@@ -221,7 +221,7 @@
      <li>
      <details>
      <summary>${item.text}</summary>
-     <ul class="p-2 z-[12] lg:min-w-32 lg:max-2-80 lg:w-max">
+     <ul class="p-2 z-[12] md:min-w-32 md:max-2-80 md:w-max">
   ${itemsTag}
      </ul>
      </details>
@@ -240,10 +240,10 @@
 
     return `
     <div class="dropdown dropdown-bottom dropdown-end">
-      <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+      <div tabindex="0" role="button" class="btn btn-ghost md:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </div>
-      <ul tabindex="0" class="dropdown-content z-[12] menu mt-3 p-2 shadow bg-base-100 rounded-box w-60">
+      <ul tabindex="0" class="dropdown-content z-[12] menu mt-3 p-2 shadow bg-base-100 rounded-box w-screen">
 ${itemsTag}
       </ul>
     </div>`;
@@ -285,7 +285,7 @@ ${itemsTag}
   function navbarCenter(items) {
     const menu = navbarMenu(items);
     return `
-  <div class="navbar-center hidden lg:flex">
+  <div class="navbar-center hidden md:flex">
 ${menu}
   </div>`;
   }
